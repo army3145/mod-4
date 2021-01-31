9b4417d49cda23b84b486810b28e8c1dca77b03c
army3145 <76448948+army3145@users.noreply.github.com>
2021-01-31 17:05:21 -0600

    Create helloscript.js

---
+++ helloscript.js
@@ -0,0 +1,7 @@
+  (function( )  {
+  	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+       for (var  i  =  0;  i  <  names.length;  i++) {
+       console.log("Hello  "   +   names[i]);
+       }
+      
+      }( )); 
